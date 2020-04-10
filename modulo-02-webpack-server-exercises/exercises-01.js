a5c91e78571b2984251793c055a3e8f45598d05b
LeonardoBrizolla <leonardobrizollars@gmail.com>
2020-04-09 23:40:29 -0300

    Exercises module 02

---
+++ modulo-02-webpack-server-exercises/exercises-01.js
@@ -0,0 +1,10 @@
+//1.1
+import ClasseUsuario from './function.js';
+ClasseUsuario.info();
+
+//1.2
+import { idade } from './function.js';
+alert(`Idade: ${idade}`);
+
+//1.3
+import ClasseUsuario, { idade as IdadeUsuario}from './function.js';
